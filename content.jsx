7f95836e4ba5c2f6ed875f682e09955bcd9b60bc
Eric King <e.king117@gmail.com>
2015-11-05 19:51:29 -0500

    Comments now generated from data object.

--- content.jsx
+++ content.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
  
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello</h1>
+class CommentBox extends React.Component {
+  render () {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<Hello/>, document.getElementById('hello'));
+class CommentList extends React.Component {
+  render () {
+    let commentNodes = this.props.data.map( (comment) => {
+      return (
+        <div className="commentList">
+          {commentNodes}
+        </div>
+      );
+    });
+
+    return(
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment (from data)</Comment>
+        <Comment author="Jordan Walke">This is *another* comment (from data)</Comment>
+      </div>
+    )
+
+  }
+};
+class CommentForm extends React.Component {
+  render () {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    )
+  }
+};
+
+class Comment extends React.Component {
+  rawMarkup () {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  };
+
+  render () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(
+  <CommentBox data={data} />,
+  document.getElementById('content')
+);
