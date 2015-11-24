ea22cb0eb5fd9507dd6d1477d34d4284a3683532
Eric King <e.king117@gmail.com>
2015-11-24 13:59:47 -0500

    Added stock cards, currently not displaying right.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,19 +28,32 @@ angular.module('starter.controllers', [])
 })
 
 .controller('MoviesCtrl', function($scope) {
+  console.log('CARDS CTRL');
   var cardTypes = [
-    { image: 'https://pbs.twimg.com/profile_images/546942133496995840/k7JAxvgq.jpeg' },
     { image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png' },
-    { image: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg' },
+    { image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png' },
+    { image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png' },
   ];
+
   $scope.cards = Array.prototype.slice.call(cardTypes, 0);
 
   $scope.cardDestroyed = function(index) {
     $scope.cards.splice(index, 1);
   };
 
-  $scope.cardSwiped = function(index) {
-    // var newCard = // new card data
-    $scope.cards.push(newCard);
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
   };
 });
