25119920be2fa6c3e822c6f3161438879152a935
Eric King <e.king117@gmail.com>
2015-12-04 15:41:48 -0500

    Added angularfire dependency

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('movieNight', ['ionic', 'movieNight.controllers', 'movieNight.services'])
+angular.module('movieNight', ['ionic', 'firebase', 'movieNight.controllers', 'movieNight.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
