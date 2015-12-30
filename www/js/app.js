5da0db9aebdbc538c4a459eaac4e2b3fcc15ec9b
Eric King <e.king117@gmail.com>
2015-12-29 19:32:19 -0500

    Removed comments

--- www/js/app.js
+++ www/js/app.js
@@ -2,15 +2,12 @@ angular.module('movieNight', ['ionic', 'firebase', 'movieNight.controllers', 'mo
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
@@ -19,13 +16,7 @@ angular.module('movieNight', ['ionic', 'firebase', 'movieNight.controllers', 'mo
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-  //currently displays, empty title div covers it.
-  //should completely cover screen (no nav bar, yes title bar)
   .state('landing', {
     url: "/landing",
     templateUrl: "templates/landing.html",
@@ -33,7 +24,7 @@ angular.module('movieNight', ['ionic', 'firebase', 'movieNight.controllers', 'mo
     onEnter: ['Auth', '$location', function (Auth, $location){
       Auth.$onAuth(function(authData) {
         if (authData){
-          Auth.authData = authData; // This will display the user's name in our view
+          Auth.authData = authData;
           $location.path('/tab/dash');
         }
       });
@@ -41,7 +32,6 @@ angular.module('movieNight', ['ionic', 'firebase', 'movieNight.controllers', 'mo
 
   })
 
-  // setup an abstract state for the tabs directive
   .state('tab', {
     url: '/tab',
     abstract: true,
@@ -51,7 +41,6 @@ angular.module('movieNight', ['ionic', 'firebase', 'movieNight.controllers', 'mo
     }
   })
 
-  // Each tab has its own nav history stack:
   .state('tab.movies', {
     url: '/movies',
     views: {
